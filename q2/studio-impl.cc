45e996c618368a23b479ab4f21b315554be7d12f
pengcarol <c.peng255@gmail.com>
2025-11-15 11:54:33 -0500

    Implement Studio class with rendering and animation

---
+++ q2/studio-impl.cc
@@ -0,0 +1,38 @@
+module Studio;
+
+Studio::Studio(AsciiArt *picture) : thePicture{picture} { }
+
+AsciiArt *&Studio::picture() {
+    return thePicture;
+}
+
+void Studio::reset() {
+    ticks = 0;
+}
+
+void Studio::render() {
+    out << '+';
+    for (int j = 0; j < cols; ++j) {
+        out << '-';
+    }
+    out << '+' << std::endl;
+    for (int i = 0; i < rows; ++i) {
+        out << '|';
+        for (int j = 0; j < cols; ++j) {
+            out << picture()->charAt(i, j, ticks);
+        }
+        out << '|' << std::endl;
+    }
+    out << '+';
+    for (int j = 0; j < cols; ++j) out << '-';
+    out << '+' << std::endl;
+        ++ticks;
+}
+
+void Studio::animate(int numTicks) {
+    for (int i = 0; i < numTicks; ++i) render();
+}
+
+Studio::~Studio() {
+    delete thePicture;
+}
